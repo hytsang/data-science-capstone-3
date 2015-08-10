20ad550c69f2c9b5d46f206a7c34c449c39476cf
Yuan Chuan Kee <kylase@outlook.com>
2015-08-10 11:37:46 +0800

    R code for Quiz 1

---
+++ Quiz/1/quiz_1.R
@@ -0,0 +1,24 @@
+setwd("/Users/yuanchuan/Dropbox/Projects/Coursera/Data Science/Capstone")
+
+wd = getwd()
+
+en_US_data_path = file.path(wd, "final", "en_US")
+files = list.files(en_US_data_path)
+
+q1_ans = file.size(file.path(en_US_data_path, files[1]))/1024^2
+
+blogs_file = file.path(en_US_data_path, files[1])
+news_file = file.path(en_US_data_path, files[2])
+twitter_file = file.path(en_US_data_path, files[3])
+
+twitter = readLines(twitter_file)
+news = readLines(news_file)
+blogs = readLines(news_file)
+
+q2_ans = length(twitter)
+
+q3_ans = max(c(max(nchar(twitter)), max(nchar(news)), max(nchar(blogs))))
+
+q4_ans = sum(grepl("love", twitter))/sum(grepl("hate", twitter))
+
+q5_ans = sum(grepl("A computer once beat me at chess, but it was no match for me at kickboxing", twitter))
