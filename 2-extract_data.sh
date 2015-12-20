4e3d5accd424863b1494b212b3fae5eeccfc121f
Yuan Chuan Kee <kylase@outlook.com>
2015-12-20 14:41:43 +0800

    Added parameter to extract English files only and removed commands to delete non-English files

--- 2-extract_data.sh
+++ 2-extract_data.sh
@@ -5,19 +5,6 @@ DATA_DIR=$WORKING_DIR/data
 
 if [ -f "$WORKING_DIR/data/Coursera-SwiftKey.zip" ]; then
   if [ ! -d "$WORKING_DIR/data/final/en_US" ]; then
-    unzip $WORKING_DIR/data/Coursera-SwiftKey.zip -d $WORKING_DIR/data/
+    unzip $WORKING_DIR/data/Coursera-SwiftKey.zip final/en_US/en_US.* -d $WORKING_DIR/data/
   fi
-fi
-
-# Delete non-English data files
-if [ -d "$WORKING_DIR/data/final/fi_FI " ]; then
-  rm -r $WORKING_DIR/data/final/fi_FI
-fi
-
-if [ -d "$WORKING_DIR/data/final/de_DE " ]; then
-  rm -r $WORKING_DIR/data/final/de_DE
-fi
-
-if [ -d "$WORKING_DIR/data/final/ru_RU " ]; then
-  rm -r $WORKING_DIR/data/final/ru_RU
 fi
